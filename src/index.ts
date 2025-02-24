c211035152b177fb04308183ee990c8ca79e9897
sneko <supernekocat31@gmail.com>
2025-02-24 14:39:16 +0900

    feat:add summarization route

--- src/index.ts
+++ src/index.ts
@@ -3,29 +3,53 @@ import { Hono } from "hono"
 const app = new Hono()
 
 export interface Env {
-  AI: any;
+    AI: Ai
 }
 
+/**
+ * Chat
+ */
 app.get('/', async (c :any) => {
-    const ai = new Ai(c.env.AI)
-
-    // URLのクエリで質問受ける
     const { text } = c.req.query()
 
     if (!text) {
         return c.text("Missing text", 400);
     }
 
-    const answer = await ai.run(
-      '@cf/meta/llama-3.1-8b-instruct',
-      {
-        messages: [
-          { role: 'user', content: text }
-        ]
-      }
+    const response = await c.env.AI.run(
+        '@cf/meta/llama-3.1-8b-instruct',
+        {
+            messages: [
+                {
+                    role: 'user',
+                    content: text
+                }
+            ]
+        }
     )
 
-    return c.json(answer)
+    return c.json(response)
+})
+
+/**
+ * Summarization
+ */
+app.get('/summarize', async (c :any) => {
+    const { text } = c.req.query()
+
+    if (!text) {
+        return c.text("Missing text", 400);
+    }
+
+    const response = await c.env.AI.run(
+        "@cf/facebook/bart-large-cnn",
+        {
+            input_text: text,
+            max_length: 32,
+        }
+    )
+
+    return c.json(response)
 })
 
 export default app
